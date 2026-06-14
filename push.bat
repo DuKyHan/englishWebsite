79a41f5df10b3a92ec7c4d7d4c88548a2e3d258a
Trương Duy Khang <DuKyHan2312@gmail.com>
2026-06-14 22:24:27 +0700

    aaa

--- push.bat
+++ push.bat
@@ -4,13 +4,16 @@ set /p MSG="Commit message (Enter = 'update'): "
 if "%MSG%"=="" set MSG=update
 
 echo.
-echo [1/3] git add .
+echo [1/4] git add .
 git add .
 
-echo [2/3] git commit -m "%MSG%"
+echo [2/4] git commit -m "%MSG%"
 git commit -m "%MSG%"
 
-echo [3/3] git push
+echo [3/4] git pull --rebase
+git pull --rebase
+
+echo [4/4] git push
 git push
 
 echo.
